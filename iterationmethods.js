4c7d7746ca7e0b90eb99af8cb271c76c5d7264a8
Abhishek527 <abhishekp.abhi84@gmail.com>
2024-12-27 11:35:12 +0530

    updatediteration

--- iterationmethods.js
+++ iterationmethods.js
@@ -18,3 +18,10 @@ console.log('even',even);
 
 let odd = numbers.filter((ele)=> ele%2 !== 0)
 console.log('odd',odd);
+
+//WAP to to find and print all even numbers from n array
+//WAP to find and add all odd elements from an array into new array.
+//WAP to find sum of all elements present in an array
+//WAP to find Product of all elements present in an array.
+//WAP to add +5 to each element of an array and print it.
+//WAP to find squuare of each element present in an array
