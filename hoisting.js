c1eeae776ba69b67c250497a69f4c79b8ca2110f
Abhishek527 <abhishekp.abhi84@gmail.com>
2025-01-02 10:03:02 +0530

    Hoisting

---
+++ hoisting.js
@@ -0,0 +1,15 @@
+add();
+sub();
+function sub() {
+    //console.log(x - y);
+    var x = 40;
+    var y = 20;
+    console.log(x - y);
+}
+
+function add() {
+    console.log(a + b);
+    var a = 10;
+    var b = 20;
+    console.log(a + b);
+}
